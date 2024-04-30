77a38a6e63ce16b7455be1821be40566ce7b9321
Bernardo Mendes <134334489+brnd09@users.noreply.github.com>
2024-04-29 23:24:47 -0300

    Create Sala.java

---
+++ Sala.java
@@ -0,0 +1,12 @@
+public class Sala {
+
+  int idsala, numAssentos;
+  String tipoTela, localizacao;
+
+  public Sala(int idsala, int numAssentos, String tipoTela, String localizacao) {
+    this.idsala = idsala;
+    this.numAssentos = numAssentos;
+    this.tipoTela = tipoTela;
+    this.localizacao = localizacao;
+  }
+}
